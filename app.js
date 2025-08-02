5627feafc098a81fada1e42ed9b7ab48f3c6c074
Avishek Kumar Pandit <avishekpandit51@gmail.com>
2025-08-02 11:12:25 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+particlesJS("particles-js", {
+  "particles": {
+    "number": {"value": 80,"density": {"enable": true,"value_area": 800}},
+    "color": {"value": ["#00ffff", "#ff00ff", "#ffff00", "#00ff00", "#ff0000", "#ffffff"]},
+    "shape": {"type": "circle"},
+    "opacity": {"value": 0.6},
+    "size": {"value": 3,"random": true},
+    "line_linked": {"enable": true,"distance": 150,"color": "#ffffff","opacity": 0.4,"width": 1},
+    "move": {"enable": true,"speed": 4}
+  },
+  "interactivity": {
+    "events": {"onhover": {"enable": true,"mode": "repulse"}, "onclick": {"enable": true,"mode": "push"}},
+    "modes": {"repulse": {"distance": 100,"duration": 0.4},"push": {"particles_nb": 4}}
+  },
+  "retina_detect": true
+});
